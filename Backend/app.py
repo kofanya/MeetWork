dfb9414432e1cb4c9908c30802f5a905de49a514
Андрей <andrejkurbatov074@gmail.com>
2026-01-06 03:12:20 +1000

    add все роли в БД и fix имеющиеся

--- Backend/app.py
+++ Backend/app.py
@@ -6,24 +6,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 db = SQLAlchemy(app)
 class User (db.Model):
     id = db.Column(db.Integer,primary_key = True)
-    user_email = db.Column(db.String, unique = True)
-    user_password_hash = db.Column(db.String, unique = True)
-    user_role = db.Column(db.String, default = 'user')
+    first_name = db.Column(db.String)
+    second_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    old = db.Column(db.Integer)
+    contact_phone = db.Column(db.String)
+    email = db.Column(db.String, unique = True)
+    password_hash = db.Column(db.String)
+    role = db.Column(db.String, default = 'applicant')
     date_created_at = db.Column(db.DateTime, default = datetime.utcnow)
     employer = db.relationship('Employer', backref = 'user', uselist = False)
+    applicant = db.relationship('Applicant', backref = 'user', uselist = False)
+    admin = db.relationship('Admin', backref = 'user', uselist = False)
     
-
 class Employer(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True)
     company_name = db.Column(db.String)
-    contact_person = db.Column(db.String)
     description_vacancy = db.Column(db.Text)
-    contact_phone = db.Column(db.Integer)
     contact_website = db.Column(db.String)
-    user_old = db.Column(db.Integer)
-    user_name = db.Column(db.String)
 
+class Applicant(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True)
+    resume = db.Column(db.Text)
+    education = db.Column(db.String)
 
+class Admin(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True)
 
 
 
